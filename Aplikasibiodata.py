20e04033926726571142ebf2b6f3100e19e36999
Prihatini21 <114629833+Prihatini21@users.noreply.github.com>
2022-09-29 11:17:39 +0700

    biodata

---
+++ Aplikasibiodata.py
@@ -0,0 +1,11 @@
+# aplikasi biodata by prihatini
+
+nama = input ("masukan nama : ")
+kelas = input ("masukan kelas ini:")
+prodi = input ("prodi saat ini")
+TTL = input ("tempat tanggal lahir")
+
+print ("nama anda", nama)
+print ("kelas saat ini",kelas)
+print ("prodi anda",prodi)
+print ("tempat tanggal lahir", TTL)
